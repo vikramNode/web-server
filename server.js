db3702cb72e53310471eadad797d652c440c4294
vikram <vikram.sethumadhavan@indegene.com>
2016-01-04 14:51:31 +0530

    todo api

--- server.js
+++ server.js
@@ -4,26 +4,51 @@ var app = express();
 
 var middleware = require('./middleware');
 
+var todos =[
+    {
+        id:1,
+        description:'go to grocery shop',
+        complete:false
+    },
+    {
+        id:2,
+        description:'go to Mall',
+        complete:false
+    }
+]
+
 app.use(middleware.requireAuth);
 
 
-//get method accepts two arguments
-app.get('/about', function(req, res){
+app.get('/todos/:id', function(req, res){
     //res.send('About Us');
-    res.sendFile(__dirname +'/public' + '/about.html');
+    //res.sendFile(__dirname +'/public' + '/about.html');
+    
+    //getting id
+    var todoId = parseInt(req.params.id);
+        //console.log(todoId);
+    var idfound;
+    //looping an array items
+    todos.forEach(function(v, i){
+        console.log(v.id);
+        if(v.id === todoId){
+            idfound = v;
+            //console.log(idfound);
+        }
+        else{
+            //res.status(404).send();
+        }
+    });
+        
+        if(idfound){
+            res.json(idfound);
+        }else{
+            res.status(404).send();
+            //res.status(404).send();
+        }
+    
+    //res.send('Asking todo id is '+ req.params.id)
 });
-
-app.get('/contact', function(req, res){
-    //res.send('About Us');
-    res.sendFile(__dirname +'/public' + '/about.html');
-});
-
-
-app.get('/contact', function(req, res){
-    //res.send('About Us');
-    res.sendFile(__dirname +'/public' + '/about.html');
-});
-
 
 app.use(express.static(__dirname + '/public'));
 
